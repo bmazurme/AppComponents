a8531f7b9b315521299b829444721f281f3c4285
BMAZUR <49879328+bmazurme@users.noreply.github.com>
2022-03-16 18:05:33 +0300

    update

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,22 +2,84 @@ import React from "react";
 import Star from "./Star";
 
 function Rating() {
-  const [stars, setStars] = React.useState([false, false, false, false, false]);
+  const [stars, setStars] = React.useState([
+    {clicked: false, focus: false, active: false},
+    {clicked: false, focus: false, active: false},
+    {clicked: false, focus: false, active: false},
+    {clicked: false, focus: false, active: false},
+    {clicked: false, focus: false, active: false}
+  ]);
 
-  function handleClick() {
-    
+  function handleClick(id) {
+    const arr = [];
+    stars.map(
+      (star, index) => {
+        if (id >= index) {
+          arr.push({
+            clicked: true,
+            focus: star.focus,
+            active: star.active
+          });
+        } else {
+          arr.push({
+            clicked: false,
+            focus: star.focus,
+            active: star.active
+          });
+        }
+      });
+    setStars(arr);
+    //console.log(arr);
+  }
 
-    //setSwitcher(!switcher);
+  function handleUnFocus(id) {
+    const arr = [];
+    stars.map(
+      (star, index) => {
+        arr.push({
+          clicked: star.clicked,
+          focus: false,
+          active: star.clicked
+        });
+      });
+    setStars(arr);
+    console.log(arr);
+  }
+
+  function handleFocus(id) {
+    const arr = [];
+    stars.map(
+      (star, index) => {
+        if (id >= index) {
+          arr.push({
+            clicked: star.clicked,
+            focus: true,
+            active: true
+          });
+        } else {
+          arr.push({
+            clicked: star.clicked,
+            focus: false,
+            active: false
+          });
+        }
+      });
+    setStars(arr);
+    console.log(arr);
   }
 
   return(
     <div className="rating">
 
       {stars.map((star, index) => 
+
         <Star key={index} 
-              active={star}
+              id={index}
+              active={star.active }
               handleClick={handleClick}
-        />
+              handleFocus={handleFocus}
+              handleUnFocus={handleUnFocus}
+        /> 
       )}
 
     </div>
--- src/components/Star.js
+++ src/components/Star.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Star(props) {
   return(
-    <div onClick={props.handleClick}
-         className={`rating__star ${props.active && 'rating__star_active'}`}>
+    <div onClick={()=>props.handleClick(props.id)}
+         onPointerEnter={()=>{props.handleFocus(props.id)}}
+         onPointerLeave={props.handleUnFocus}
+
+         className={`rating__star ${ props.active && 'rating__star_active'}`}>
     </div>
   );
 }
