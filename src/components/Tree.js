71970617d03c8702ab8351bb52f866e95c85cde1
Bogdan <b@bmazur.ru>
2022-03-19 23:30:56 +0300

    update

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,24 +1,39 @@
+import React from "react";
 import { materials } from "./initData";
 
 function Tree() {
-  return (
-        <select className="popup__tree" name="pets" multiple size="1">
-          {materials.map((material, index) =>         
-            <optgroup key={index} label={`${material.group} ${material.children.length}` }>
-              {
-                material.children.map((item, i) => 
-                
-                  <option key={`${index}_${i}`} 
-                          value={item.name}
-                  >
+  function getCount() {
+    let i = 0;
+    materials.forEach(item => { 
+      i += item.children.length;
+    });
+    return i;
+  }
 
-                    {item.name}
-                    
-                  </option>
-                )}
-            </optgroup>
-          )}
-        </select>
+  return (
+    <div className="popup__tree">
+      <div className="tree__group">
+        <input className="tree__input" type="checkbox"></input>
+          <span className="tree__label">All</span>  
+          <span className="tree__count">({getCount()})</span> 
+      </div>
+
+    {materials.map((material, index) =>         
+      <div className="tree__group" key={index}>
+        <input className="tree__input" type="checkbox"></input>
+          <span className="tree__label">{material.group}</span>  
+          <span className="tree__count">({material.children.length})</span>   
+          {
+            material.children.map((item, i) => 
+              <div className="tree__item" key={`${index}_${i}`} value={item.name} >
+                <input className="tree__input" type="checkbox"></input>
+                {item.name}  
+              </div>
+            )}
+            </div>
+          )} 
+        </div>
+
   );
 }
 
